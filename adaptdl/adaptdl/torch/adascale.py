fix adascale variance bug, add NCF

adaptdl/adaptdl/torch/adascale.py
@@ -20,6 +20,7 @@ import torch.distributed
 import torch.optim
 from torch.autograd import Variable
 
+
 __all__ = ["AdaScale"]
 
 
@@ -209,9 +210,9 @@ class AdaScale(object):
             self._norms = torch.zeros((self._accumulation_steps,
                                        self._num_params),
                                       device=grad[0].device)
-        grad /= self._accumulation_steps
         self._norms[self._current_accumulation_step][idx] = \
             grad.pow(2).sum()
+        grad /= self._accumulation_steps
         self._final_callback_queued = False
         Variable._execution_engine.queue_callback(self._queue_callback)
 
@@ -232,15 +233,15 @@ class AdaScale(object):
         # This method should be invoked once for each backward pass, after
         # gradients have been synchronized between each replica.
         self._final_callback_queued = False
+        if (self.is_accumulation_step()):
+            return
         grad = []
         total_steps = self._accumulation_steps
         for group in self._optimizer.param_groups:
-            grad.extend([p.grad.detach().clone() / total_steps
+            grad.extend([p.grad.detach().clone()
                          if p.grad is not None else
                          torch.zeros([1], dtype=torch.float64)
                          for p in group["params"]])
-        if (self.is_accumulation_step()):
-            return
 
         theta = self._smoothing ** self._scale
         has_previous_step = not (self._norms_future is None)
@@ -267,8 +268,7 @@ class AdaScale(object):
                 # DistributedDataParallel averages gradients across replica,
                 # but we also need to average the gradients across the
                 # gradient accumulation steps manually
-                n = _normsq(
-                    [g for g in self._prev_full_grad])
+                n = _normsq(self._prev_full_grad)
                 var = norms / (samples - 1)
                 var -= n * (samples / (samples - 1))
                 var *= (self._scale / samples)

examples/NCF/data_utils.py
@@ -1,3 +1,5 @@
+# Code adapted from https://github.com/guoyang9/NCF
+
 import numpy as np 
 import pandas as pd 
 import scipy.sparse as sp
@@ -5,30 +7,28 @@ import os
 import torch.utils.data as data
 import hashlib
 
-import config
 import adaptdl.env
 from urllib.request import urlretrieve
 
 
 base_url = "https://raw.githubusercontent.com/hexiangnan/neural_collaborative_filtering/312aff0ee6f4df0ba60041329a74ed29bc7ac4b4/Data/"
 
-def download_all(dataset='pinterest-20'):#"ml-1m"):
-    root = config.main_path
+def download_all(root_path, dataset="ml-1m"):
     datafiles = ['{}.{}'.format(dataset, suffix) for suffix in
                  ['train.rating', 'test.rating', 'test.negative']]
     for filename in datafiles:
-        local_filename = os.path.join(root, filename)
+        local_filename = os.path.join(root_path, filename)
         # Todo: verify file
         if not os.path.exists(local_filename):
             print(base_url + filename, local_filename)
             urlretrieve(base_url + filename, local_filename)
 
 
-def load_all(test_num=100):
+def load_all(root_path, train_rating, test_negative, test_num=100):
     """ We load all the three file here to save time in each epoch. """
-    download_all()
+    download_all(root_path)
     train_data = pd.read_csv(
-        config.train_rating, 
+        train_rating, 
         sep='\t', header=None, names=['user', 'item'], 
         usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
 
@@ -43,7 +43,7 @@ def load_all(test_num=100):
         train_mat[x[0], x[1]] = 1.0
 
     test_data = []
-    with open(config.test_negative, 'r') as fd:
+    with open(test_negative, 'r') as fd:
         line = fd.readline()
         while line != None and line != '':
             arr = line.split('\t')

examples/NCF/main.py
@@ -1,21 +1,24 @@
+# Code adapted from https://github.com/guoyang9/NCF
+
 import os
 import time
 import argparse
 import numpy as np
 
 import torch
-import torch.nn as nn
+import torch.nn
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 from torch.utils.tensorboard import SummaryWriter
 
 import model
-import config
 import evaluate
 import data_utils
 
 import adaptdl
 import adaptdl.torch as adl
+import adaptdl.env
+import os.path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--lr",
@@ -24,15 +27,15 @@ parser.add_argument("--lr",
                     help="learning rate")
 parser.add_argument("--dropout",
                     type=float,
-                    default=0.0,  
+                    default=0.1,
                     help="dropout rate")
-parser.add_argument("--batch_size", 
+parser.add_argument("--batch_size",
                     type=int,
-                    default=64, 
+                    default=256,
                     help="batch size for training")
 parser.add_argument("--epochs",
                     type=int,
-                    default=20,  
+                    default=40,
                     help="training epoches")
 parser.add_argument("--top_k",
                     type=int,
@@ -66,75 +69,106 @@ parser.add_argument("--autoscale-bsz",
                     default=False,
                     action='store_true',
                     help="Use AdaptDL batchsize autoscaling")
+parser.add_argument("--gradient-accumulation",
+                    dest='gradient_accumulation',
+                    default=False,
+                    action='store_true',
+                    help="Use AdaptDL batchsize autoscaling")
+parser.add_argument("--dataset",
+                    type=str,
+                    choices=['ml-1m', 'pinterest-20'],
+                    default="ml-1m")
+parser.add_argument("--model-type",
+                    dest="model_type",
+                    type=str,
+                    choices=['MLP', 'GMF', 'NeuMF-end', 'NeuMF-pre'],
+                    default="NeuMF-end")
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 cudnn.benchmark = True
 
+dataset = args.dataset
+model_type = args.model_type
+
+# paths
+main_path = adaptdl.env.share_path()
+
+train_rating = os.path.join(main_path, '{}.train.rating'.format(dataset))
+test_rating = os.path.join(main_path, '{}.test.rating'.format(dataset))
+test_negative = os.path.join(main_path, '{}.test.negative'.format(dataset))
+
+model_path = os.path.join(main_path, 'models')
+GMF_model_path = os.path.join(model_path, 'GMF.pth')
+MLP_model_path = os.path.join(model_path, 'MLP.pth')
+NeuMF_model_path = os.path.join(model_path, 'NeuMF.pth')
 
 ############################## PREPARE DATASET ##########################
-train_data, test_data, user_num ,item_num, train_mat = data_utils.load_all()
+train_data, test_data, user_num, item_num, train_mat = \
+    data_utils.load_all(main_path, train_rating, test_negative)
 
 # construct the train and test datasets
 train_dataset = data_utils.NCFData(
         train_data, item_num, train_mat, args.num_ng, True)
 test_dataset = data_utils.NCFData(
         test_data, item_num, train_mat, 0, False)
-train_loader = adl.AdaptiveDataLoader(train_dataset,
-        batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
-test_loader = adl.AdaptiveDataLoader(test_dataset,
-        batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
+train_loader = adl.AdaptiveDataLoader(
+    train_dataset,
+    batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
+test_loader = adl.AdaptiveDataLoader(
+    test_dataset,
+    batch_size=args.test_num_ng+1, shuffle=False, num_workers=0)
 
 if args.autoscale_bsz:
-    train_loader.autoscale_batch_size(8192, local_bsz_bounds=(32, 64), gradient_accumulation=True)
+    train_loader.autoscale_batch_size(
+        8192, local_bsz_bounds=(32, 512),
+        gradient_accumulation=args.gradient_accumulation)
 
 ########################### CREATE MODEL #################################
-if config.model == 'NeuMF-pre':
-    assert os.path.exists(config.GMF_model_path), 'lack of GMF model'
-    assert os.path.exists(config.MLP_model_path), 'lack of MLP model'
-    GMF_model = torch.load(config.GMF_model_path)
-    MLP_model = torch.load(config.MLP_model_path)
+if model_type == 'NeuMF-pre':
+    assert os.path.exists(GMF_model_path), 'lack of GMF model'
+    assert os.path.exists(MLP_model_path), 'lack of MLP model'
+    GMF_model = torch.load(GMF_model_path)
+    MLP_model = torch.load(MLP_model_path)
 else:
     GMF_model = None
     MLP_model = None
 
-model = model.NCF(user_num, item_num, args.factor_num, args.num_layers, 
-                        args.dropout, config.model, GMF_model, MLP_model)
-model.cuda()
-loss_function = nn.BCEWithLogitsLoss()
-
-if config.model == 'NeuMF-pre':
-    optimizer = optim.SGD(model.parameters(), lr=args.lr)
-else:
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
-# writer = SummaryWriter() # for visualization
+network = model.NCF(user_num, item_num, args.factor_num, args.num_layers,
+                    args.dropout, model_type, GMF_model, MLP_model)
 adaptdl.torch.init_process_group("nccl" if torch.cuda.is_available()
-                                     else "gloo")
-model = adl.AdaptiveDataParallel(model, optimizer)
+                                 else "gloo")
+network.cuda()
+loss_function = torch.nn.BCEWithLogitsLoss()
+
+if model_type == 'NeuMF-pre':
+    optimizer = optim.SGD(network.parameters(), lr=args.lr)
+else:
+    optimizer = optim.Adam(network.parameters(), lr=args.lr)
+network = adl.AdaptiveDataParallel(network, optimizer,
+                                   find_unused_parameters=True)
 ########################### TRAINING #####################################
 count, best_hr = 0, 0
 tensorboard_dir = os.path.join(os.getenv("ADAPTDL_TENSORBOARD_LOGDIR", "/tmp"),
                                adaptdl.env.job_id())
 with SummaryWriter(tensorboard_dir) as writer:
     for epoch in adl.remaining_epochs_until(args.epochs):
-        model.train() # Enable dropout (if have).
+        network.train()  # Enable dropout (if have).
         start_time = time.time()
         train_loader.dataset.ng_sample()
- 
+        gain = 0
         for user, item, label in train_loader:
             user = user.cuda()
             item = item.cuda()
             label = label.float().cuda()
- 
-            model.zero_grad()
-            prediction = model(user, item)
+
+            network.zero_grad()
+            prediction = network(user, item)
             loss = loss_function(prediction, label)
             loss.backward()
             optimizer.step()
-            # writer.add_scalar('data/loss', loss.item(), epoch)
-        #    count += 1
-            gain = model.gain
+            count += 1
+            gain = network.gain
             batchsize = train_loader.current_batch_size
             accumulation_steps = train_loader.accumulation_steps
 
@@ -143,22 +177,29 @@ with SummaryWriter(tensorboard_dir) as writer:
                           batchsize, epoch)
         writer.add_scalar("Throughput/Accumulation_Steps",
                           accumulation_steps, epoch)
- 
-        model.eval()
-        HR, NDCG = evaluate.metrics(model, test_loader, args.top_k)
- 
+
+        network.eval()
+        stats = adl.Accumulator()
+        HR, NDCG = evaluate.metrics(network, test_loader, args.top_k)
+        stats['HR'] += HR
+        stats['replicas'] += 1.0
+        with stats.synchronized():
+            writer.add_scalar('Loss/HR', stats['HR'] / stats['replicas'],
+                              epoch)
+
         elapsed_time = time.time() - start_time
-        print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
-                time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
+        print("The time elapse of epoch {:03d}".format(epoch) + " is: " +
+              time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
         print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
- 
+
         if HR > best_hr:
             best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
             if args.out and adaptdl.env.replica_rank() == 0:
-                if not os.path.exists(config.model_path):
-                    os.mkdir(config.model_path)
-                torch.save(model._state.model, 
-                    '{}{}.pth'.format(config.model_path, config.model))
+                if not os.path.exists(model_path):
+                    os.mkdir(model_path)
+                torch.save(
+                    network._state.model,
+                    '{}/{}.pth'.format(model_path, model_type))
 
 print("End. Best epoch {:03d}: HR = {:.3f}, NDCG = {:.3f}".format(
-                                    best_epoch, best_hr, best_ndcg))
+      best_epoch, best_hr, best_ndcg))